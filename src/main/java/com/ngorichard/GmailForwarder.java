ccd009acdb78537aeddd9ff1fae264312789020e
Richard Ngo <rngo@xtivia.com>
2020-08-16 20:11:40 -0500

    set port to 8888 for callback url

--- src/main/java/com/ngorichard/GmailForwarder.java
+++ src/main/java/com/ngorichard/GmailForwarder.java
@@ -73,7 +73,8 @@ public class GmailForwarder {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
     private static String fromLabelId;
